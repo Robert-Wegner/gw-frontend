8f7ffad599427631a5f86ff8ddc30d7c5a27bd55
Robert Wegner <robert.wegner4@outlook.de>
2026-08-27 15:01:32 +0200

    Increase spacing above planet status icons

--- src/components/Satellite.js
+++ src/components/Satellite.js
@@ -62,7 +62,7 @@ class Satellite extends React.Component {
         {statusIcon && (
           <StatusBar
             height={statusLogoSize}
-            distance={objectSize * 0.5 + 5}
+            distance={(objectSize * 0.5 + 5) * 1.5}
             contents={[<img key="status" alt={this.props.status} src={statusIcon} style={{ height: "100%", width: "auto", maxWidth: statusLogoSize * 1.8 }} />]}
           />
         )}
