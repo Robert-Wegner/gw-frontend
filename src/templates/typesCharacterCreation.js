8831bbc38c4ad5a446a275c59e198a6c180cd6ef
Robert Wegner <robert.wegner4@outlook.de>
2026-08-27 14:31:16 +0200

    Remove unused backend and CRA scaffolding

--- src/templates/typesCharacterCreation.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-
-var propTypesTemplate = {
-   suggestedName: PropTypes.string.isRequired,
-   requestName: PropTypes.func.isRequired,
-   submitFunction: PropTypes.func.isRequired
-}
-
-export {propTypesTemplate}
